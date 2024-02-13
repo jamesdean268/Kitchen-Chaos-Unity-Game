40a8947c87a43b2ea3a68456ffc3795ddcd27057
James Dean <jamesdean268@gmail.com>
2024-02-13 14:02:13 +1030

    Implemented an interface for an IKitchenObjectParent to enable a kitchen object to be held by any parent that implements the interface (counter and player)

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -2,11 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KitchenObject : MonoBehaviour
-{
+public class KitchenObject : MonoBehaviour {
+
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    private IKitchenObjectParent kitchenObjectParent;
 
     public KitchenObjectSO GetKitchenObjectSO() {
         return kitchenObjectSO;
+    }
+
+    public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent) {
+        // If there is already a parent, remove the kitchen object from it
+        if (this.kitchenObjectParent != null) {
+            this.kitchenObjectParent.ClearKitchenObject();
+        }
+        
+        // Assign the new parent
+        this.kitchenObjectParent = kitchenObjectParent;
+        if (kitchenObjectParent.HasKitchenObject()) {
+            Debug.LogError("IKitchenObjectParent already has a KitchenObject!");
+        }
+        kitchenObjectParent.SetKitchenObject(this);
+
+        // Update the transform / visual to the new parent
+        transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public IKitchenObjectParent GetKitchenObjectParent() {
+        return kitchenObjectParent;
     }
 }
